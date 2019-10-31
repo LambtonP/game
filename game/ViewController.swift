457abd992c068818549fd64284bf3755c501f4ba
Simranjeet kaur <chakkalsimmi@gmail.com>
2019-10-31 14:18:11 -0400

    creating emojis and variables

--- game/ViewController.swift
+++ game/ViewController.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var choices: [UIButton]!
+    
+    var points = 5
+    var lives = 0
+    
+    var stuff:[(name:String, emoji:String)]?
+    var seconds = 5
+    var timer:Timer?
+    var istimerunning = false
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        stuff = [("Dog","🐶"),("Cat","🐱"),("Mice","🐭"),("Fox","🦊"),("Rooster","🐔"),("Panda","🐼"),("Pumpkin","🎃"),("Ghost","👻"),("Dice","🎲"),("skelton","💀"),("frog","🐸"),("unicorn","🦄"),("snake","🐍"),("lizard","🦎"),("blowfish","🐡")]
     }
 
 
